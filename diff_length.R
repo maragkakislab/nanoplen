609d59afd23eb653acd9ec03c656325a68ed41b5
Chris Lee <chris.lee@nih.gov>
2021-07-07 14:22:11 -0500

    Fixed many bugs, updated help and comments

--- diff_length.R
+++ diff_length.R
@@ -1,31 +1,35 @@
 #!/usr/bin/env Rscript
 
+# Debug test:
+# dev/diff_length.R -d scratch/plen_test_data.tab -b "control" -l F -m scratch/plen_test_metadata.tab
+
 suppressPackageStartupMessages(library(optparse))
 
 option_list <- list(
-    make_option(c("-p","--data"),
-                help="Path to data file"),
-    make_option(c("-m","--metadata"),
-                help="Path to metadata file"),
+    make_option(c("-d","--data_path"),
+                help="Path to data file, long format, columns: library id, gene/transcript id, length"),
+    make_option(c("-m","--metadata_path"),
+                help="Path to metadata file, columns: library id, condition, [additional columns]"),
     make_option(c("-t","--test"), default = "t",
-                help="Statistical test to use [default %default]"),
+                help="Statistical test to use (t:t-test, m:linear mixed model, ks:kolmogorov-smirov, w:wilcoxon) [default %default]"),
     make_option(c("-b","--baseline"), default = "Control",
                 help="String to specify baseline category [default %default]"),
     make_option(c("-l","--logscale"), default = "TRUE",
-                help="Convert  [default %default]"),
-    make_option(c("-o","--ofile"), default = "stdout()",
+                help="Convert length to log2 scale [default %default]"),
+    make_option(c("-o","--ofile"), default = "stdout",
                 help="Path to output file [default %default]")
 )
+#FIXME: option for custom model in the future
 opt <- parse_args(OptionParser(option_list = option_list))
 
 vars = c("data_path", "metadata_path","test","baseline","logscale","ofile")
-for (i in length(vars)) {
-    assign(vars[i],opt[i])
+for (i in 1:length(vars)) {
+    assign(vars[i],opt[[vars[i]]])
 }
 delim <- "\t"  #Because this script is directly after nanoplen, we can control the output
-
+ 
 if (!(test %in% c("t","w","ks","m"))) {
-    stop(sprintf("Unsupported test: %s", test))
+    stop(sprintf("Unsupported test: %s. Accepted options: t, m, w, ks", test))
 }
 
 if (test %in% c("ks","w")) {
@@ -37,10 +41,11 @@ data_file <- read.delim(data_path, header = TRUE, sep = delim)
 metadata <- read.delim(metadata_path, header = TRUE, sep = delim)
 
 #FIXME: Can omit this step if previous output names columns consistently
-colnames(data_file) = c("lib_id","transcript","Y")
+colnames(data_file) = c("lib_id","name","length")
+colnames(metadata) = c("lib_id", "condition")
 
 #Add condition column from metadata
-data_file$condition = sapply(data_file[,1], function(x) {metadata[metadata[,1] == x,2]})
+data_file$condition = sapply(data_file$lib_id, function(x) {metadata$condition[metadata$lib_id == x]})
 
 # Relevel data_file$condition to use baseline string
 data_file = within(data_file, condition <- relevel(condition, ref = baseline))
@@ -50,14 +55,14 @@ diff_length_single = function(data_file_sub, test, logscale = TRUE) {
     #FIXME: tryCatch for potential errors
     out = c(NA,NA)
     if (logscale) {
-        data_file_sub$Y = log2(data_file_sub$Y+1)
+        data_file_sub$length = log2(data_file_sub$length+1)
         est_head = "log2FC"
     } else {
         est_head = "meandiff"
     }
     tryCatch({  
         if (test == "t") {
-            res = lm(Y~condition, data = data_file_sub)
+            res = lm(length~condition, data = data_file_sub)
             out = summary(res)$coefficients[2,c(1,4)]
             names(out) = c(est_head, "pvalue")            
         } else if (test == "m") {
@@ -72,8 +77,6 @@ diff_length_single = function(data_file_sub, test, logscale = TRUE) {
         }}
     )
     
-    out$qvalue = p.adjust(out$pvalue, n = "BH")
-
     return(out)
 }
 
@@ -83,10 +86,18 @@ diff_length = function(data_file, test) {
     data_file_bygene = split(data_file, data_file[,2])
     
     out = lapply(data_file_bygene, function(d) {diff_length_single(d, test)})
-    out = do.call(rbind, out)
+    out = data.frame(do.call(rbind, out))
+    out$qvalue = p.adjust(out$pvalue,method = "BH")
     return(out)
 }
 
 outres = diff_length(data_file, test)
+outres = cbind(rownames(outres),outres)
+colnames(outres)[1] = "name"
 
-write.table(outres, file = ofile,sep = delim, quote = F, row.names = T, col.names = T)
+if (ofile == "stdout") {
+    outstring = readr::format_tsv(outres)
+    write(outstring,stdout())
+} else {
+    write.table(outres, file = ofile,sep = delim, quote = F, row.names = FALSE, col.names = TRUE)
+}
