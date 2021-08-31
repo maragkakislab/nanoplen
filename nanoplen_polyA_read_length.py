2eec936b6125d422415c9d0b349efab532d5a275
darsa <showkat.dar@nih.gov>
2021-08-31 10:11:42 -0500

    nanoplen_polyA_read_length.py: optional arguments corrected for optional column and its values

--- nanoplen_polyA_read_length.py
+++ nanoplen_polyA_read_length.py
@@ -2,9 +2,9 @@
 
 """
 Reads the nanopolish output file and prints the reference sequence-id and the
-poly-A length for each "PASS" mapped read. An additional column will be
-printed at the beginning via the  ag=rgument '--opt-col-name', with values
-from argument '--opt-col-val'.
+poly-A length for each "PASS" mapped read and polyA length greaterthan equalto
+ 1. An additional column will be printed at the beginning via the  agrgument
+ '--opt-col-name', with values in rows from argument '--opt-col-val'.
 """
 
 import sys
@@ -16,11 +16,11 @@ parser.add_argument("-i", "--ifile",
                     help="Input file from Nanopolish with polyA information")
 parser.add_argument("-r", "--ref-col-name", default="reference",
                     help="Name for column for reference ids (default: %(default)s)")
-parser.add_argument("-c", "--polya-len-col-name", default="polyA",
+parser.add_argument("-c", "--polya-len-col-name", default="polya_length",
                     help="Name for read length values column (default: %(default)s)")
-parser.add_argument("-n", "--opt-col-name", default="sample",
+parser.add_argument("-n", "--opt-col-name", required=False,
                     help="Name of an optional column (default: %(default)s)")
-parser.add_argument("-v", "--opt-col-val", default="Library-1",
+parser.add_argument("-v", "--opt-col-val", required=False,
                     help="Value for respective optional column (default: %(default)s)")
 parser.add_argument("-d", "--delimiter", default="\t",
                     help="Column delimiter of output-file, default : TAB")
@@ -34,8 +34,8 @@ else:
 #Load data to dataframe
 df = pd.read_csv(ifile, sep=args.delimiter, usecols=['contig', 'polya_length', 'qc_tag'])
 
-#select only PASS values (valid values as per literature) and length > 1 bp.
-df = df[((df["qc_tag"] == "PASS") & (df["polya_length"] > 1))]
+#select only PASS values (valid values as per literature) and length >= 1 bp.
+df = df[((df["qc_tag"] == "PASS") & (df["polya_length"] >= 1))]
 
 # Option to rename the column names
 if args.ref_col_name:
@@ -43,12 +43,9 @@ if args.ref_col_name:
 if args.polya_len_col_name:
     df = df.rename(columns={"polya_length": args.polya_len_col_name})
 
-# Add an extra column with user provided columna name and its row-values
+# Add an extra column that is provided by user, column-name and its row-values
 if args.opt_col_name and args.opt_col_val:
     df.insert(0, args.opt_col_name, args.opt_col_val)
-else:
-    print("Please provide input for both optional agruments")
-    print(sys.exit(0))
 
 #Drop extra column with QC tag
 df = df.drop('qc_tag', 1)
